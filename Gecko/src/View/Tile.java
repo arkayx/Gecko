ca3eadb043d9080b600c49986c5df432387033a3
arkayx <arkayx@gmail.com>
2015-07-13 15:47:24 +0200

    Update Tile.java

--- Gecko/src/View/Tile.java
+++ Gecko/src/View/Tile.java
@@ -27,7 +27,7 @@ public class Tile extends JComponent{
 	}
 
 	private void ComputeTilePoints(){
-		xA = parentWidth / 2 + (tileWidth * x) / 2 +  - (tileWidth * y) / 2 + offsetX;
+		xA = parentWidth / 2 + (tileWidth * x) / 2 - (tileWidth * y) / 2 + offsetX;
 		xB = xA + tileWidth / 2;
 		xC = xA;
 		xD = xA - tileWidth / 2;
